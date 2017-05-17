4416080a0bd2d1e88f252f6c97a5dace80c99232
Anton Romanov <anton.v.romanov@gmail.com>
2017-05-17 14:13:54 +0800

    add parse_size method

--- Lib/Strings.php
+++ Lib/Strings.php
@@ -255,4 +255,22 @@ class Strings {
         }
         return $str;
     }
+
+
+    /**
+     * Source from http://stackoverflow.com/a/25370978/3419751
+     * @param $size
+     * @return float
+     */
+    static function parse_size($size) {
+        $unit = preg_replace('/[^bkmgtpezy]/i', '', $size); // Remove the non-unit characters from the size.
+        $size = preg_replace('/[^0-9\.]/', '', $size); // Remove the non-numeric characters from the size.
+        if ($unit) {
+            // Find the position of the unit in the ordered string which is the power of magnitude to multiply a kilobyte by.
+            return round($size * pow(1024, stripos('bkmgtpezy', $unit[0])));
+        }
+        else {
+            return round($size);
+        }
+    }
 } 
